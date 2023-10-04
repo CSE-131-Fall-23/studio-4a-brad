9a827db58be18997eae5ad28e3de483ae5354b8e
zacharytamburri <zacharytamburri@localhost>
2023-10-04 15:05:51 -0500

    studio4

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,9 +1,8 @@
 package studio4;
-
+import java.util.Scanner;
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
@@ -12,6 +11,7 @@ import edu.princeton.cs.introcs.StdDraw;
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
+
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -19,6 +19,28 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		System.out.println(in);
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean fill = in.nextBoolean();
+		if (shape == "rectangle")
+		{
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			StdDraw.setPenColor(red,green,blue);
+			StdDraw.rectangle(x,y, halfWidth, halfHeight);
+			StdDraw.show();
+		}
+
 		
+		
+		
+//		StdDraw.setPenColor(Color.BLUE);
+//		StdDraw.rectangle(1.0, 1.0, 1.0, 1.0);
+	
 	}
 }
